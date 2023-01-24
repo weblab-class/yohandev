7cb355a3e4d8ab77d9d722b29d0fd38d6ef07624
Yohan Guyomard <yohang@mit.edu>
2023-01-24 10:18:46 -0500

    Added `MongoDB`

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,17 +2,26 @@ import { join, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
 import geckos from "@geckos.io/server";
+import mongoose from "mongoose";
 import express from "express";
 import http from "node:http";
 
 import { game } from "../platform/node";
 
 const ROOT = join(dirname(fileURLToPath(import.meta.url)), "..");
+const DB = `mongodb+srv://Yohan:${require("./db.txt")}@cluster0.r0khpr8.mongodb.net/?retryWrites=true&w=majority`;
 
+// -- API --
 const app = express()
     .use("/", express.static(join(ROOT, "build/pkg/")))
     .use("/assets/", express.static(join(ROOT, "src/assets/")));
+// -- HTTP Server --
 const server = http.createServer(app);
+// -- Database --
+const db = await mongoose
+    .set("strictQuery", true)
+    .connect(DB);
+// -- UDP Socket --
 const io = geckos();
 
 io.addServer(server);
